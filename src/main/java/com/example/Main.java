7b8df63d8f4810e2d55ff320c4ef4652dad78949
SebastianSubra <sebastiansubra@gmail.com>
2025-10-09 19:48:25 +0200

    9Oct

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        // Sample Orders
+        log.info("Starting Order Management System...");
+    }
+}
